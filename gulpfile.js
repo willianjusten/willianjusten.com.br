3f69199fb54a45696afaaaf8fed5df3af695b644
willianjusten <willianjustenqui@gmail.com>
2014-12-30 03:03:34 -0200

    Add Stylus and JS src

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp        = require('gulp'),
     plumber     = require('gulp-plumber'),
     browserSync = require('browser-sync'),
     stylus      = require('gulp-stylus'),
+    uglify      = require('gulp-uglify'),
+    concat      = require('gulp-concat'),
     jeet        = require('jeet'),
     rupture     = require('rupture'),
     koutoSwiss  = require('kouto-swiss'),
@@ -49,15 +51,29 @@ gulp.task('stylus', function(){
             use:[koutoSwiss(), prefixer(), jeet(),rupture()],
             compress: true
         }))
-        .pipe(gulp.dest('build/css'))
+        .pipe(gulp.dest('assets/css'))
 });
 
+
 /**
- * Watch scss files for changes & recompile
+ * Javascript Task
+ */
+gulp.task('js', function(){
+    return gulp.src('src/js/**/*.js')
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/'))
+});
+
+
+/**
+ * Watch stylus files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    // gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('src/styl/**/*.styl', ['stylus']);
+    gulp.watch('src/js/**/*.js', ['js']);
     gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
@@ -65,4 +81,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
