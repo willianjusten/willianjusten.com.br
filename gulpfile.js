560fcdb45ae0730ef6e1dc49666a465ebf1a472f
willianjusten <willianjustenqui@gmail.com>
2014-12-31 10:33:22 -0200

    Add _includes to gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ gulp.task('watch', function () {
     gulp.watch('src/styl/**/*.styl', ['stylus']);
     gulp.watch('src/js/**/*.js', ['js']);
      gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
