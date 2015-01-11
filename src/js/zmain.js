78bf27ad4282152b09a1b4bc511767f1b5d82132
willianjusten <willianjustenqui@gmail.com>
2015-01-11 17:28:21 -0200

    Change some styles for search

--- src/js/zmain.js
+++ src/js/zmain.js
@@ -1,16 +1,51 @@
+// Polyfill VH
 window.viewportUnitsBuggyfill.init({hacks: window.viewportUnitsBuggyfillHacks});;
 smoothScroll.init({
     updateURL: false
 })
 
+// Menu
 $("a#slide").click(function(){
     $("#sidebar,a#slide,#fade").addClass("slide");
     $("#open").hide();
+    $("#search").hide();
     $("#close").show();
 });
 
 $("#fade").click(function(){
     $("#sidebar,a#slide,#fade").removeClass("slide");
     $("#open").show();
+    $("#search").show();
     $("#close").hide();
 });
+
+// Search
+$(document).ready(function() {
+      $('.search-field').simpleJekyllSearch({
+          jsonFile : '/search.json',
+          searchResults : '.search-results',
+          template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>'
+        });
+  });
+  (function( $, window, undefined ) {
+
+     var bs = {
+          close: $(".icon-remove-sign"),
+          searchform: $(".search-form"),
+          canvas: $("body"),
+          dothis: $('.dosearch')
+      };
+
+    bs.dothis.on('click', function() {
+      $('.search-wrapper').css({ display: "block" });
+      bs.searchform.toggleClass('active');
+      bs.searchform.find('input').focus();
+      bs.canvas.toggleClass('search-overlay');
+    });
+
+      bs.close.on('click', function() {
+        $('.search-wrapper').removeAttr( 'style' );
+        bs.searchform.toggleClass('active');
+        bs.canvas.removeClass('search-overlay');
+    });
+  })( jQuery, window );
