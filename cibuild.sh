8e8b45d0c1f74f261da461a08b16ffd8e39ea341
Willian Justen <willianjustenqui@gmail.com>
2015-12-07 15:57:26 -0200

    Change to ignore code school from html-proofer

--- cibuild.sh
+++ cibuild.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ./_site
+bundle exec htmlproof ./_site --href-ignore https://www.codeschool.com/
