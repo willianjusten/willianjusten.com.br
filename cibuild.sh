0c4d02d050908e430ba642f2c97ae4bc2224f399
Willian Justen <willianjustenqui@gmail.com>
2015-12-18 15:43:02 -0200

    Remove github url to proofer

--- cibuild.sh
+++ cibuild.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ./_site --href-ignore https://www.codeschool.com/ https://github.com/LFeh/1-post-por-dia/issues/20
+bundle exec htmlproof ./_site --href-ignore https://github.com/LFeh/1-post-por-dia/issues/20
