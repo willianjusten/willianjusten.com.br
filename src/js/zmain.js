1ee2de4d7f1767b079d030dca20d7bc71d94ba5c
Willian Justen <willianjustenqui@gmail.com>
2015-12-29 16:21:47 -0200

    Add scroll animation

--- src/js/zmain.js
+++ src/js/zmain.js
@@ -45,8 +45,10 @@
       }
   });
 
-  // Scroll
-  smoothScroll.init({
-    updateURL: false
-  })
+  new AnimOnScroll( document.getElementById( 'grid' ), {
+    minDuration : 0.4,
+    maxDuration : 0.7,
+    viewportFactor : 0.2
+  } );
+
 })( Zepto, window );
