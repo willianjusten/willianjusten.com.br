f2fd4aab78a67e65de1040160a3dc40eeeb3e7c1
Willian Justen <willianjustenqui@gmail.com>
2016-01-22 01:41:34 -0200

    Data-ignore linkedin

--- cibuild.sh
+++ cibuild.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ./_site --href-ignore https://github.com/LFeh/1-post-por-dia/issues/20 https://br.linkedin.com/in/willianjusten
+bundle exec htmlproof ./_site --href-ignore https://github.com/LFeh/1-post-por-dia/issues/20
