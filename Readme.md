02dd56f0226a527a08b9bed038a81aadf8596d8b
Willian Justen <willianjustenqui@gmail.com>
2016-09-03 18:29:36 -0300

    Removing Travis for a while

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 # Willian Justen - Blog
-Personal website/blog - [![Build Status](https://travis-ci.org/willianjusten/willianjusten.github.io.svg)](https://travis-ci.org/willianjusten/willianjusten.github.io)   
+Personal website/blog
--- cibuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-echo 'Testing travis...'
-bundle exec travis-lint
-bundle exec jekyll build
-bundle exec htmlproof ./_site --href-ignore https://github.com/LFeh/1-post-por-dia/issues/20
