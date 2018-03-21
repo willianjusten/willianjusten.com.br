0e2305130eeec851dcf084c089e3066ba48566bf
Willian Justen <willianjustenqui@gmail.com>
2018-03-21 18:27:43 -0300

    Change htmlproofer to ignore status 0

--- testlinks.sh
+++ testlinks.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer ./_site --http-status-ignore 0
