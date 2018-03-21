10f2b56618764d0666ebad8098def255d4586412
Willian Justen <willianjustenqui@gmail.com>
2018-03-21 18:49:21 -0300

    Tunning htmlproofer

--- testlinks.sh
+++ testlinks.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --http-status-ignore [0, 999] --url-ignore 'https://www.linkedin.com/in/willianjusten'
+bundle exec htmlproofer ./_site --http-status-ignore "0,999" --url-ignore "/linkedin.com/,/t.co/"
