eeb36988c93d3dedb6c180b5e4b4b66d16b99a43
Willian Justen <willianjustenqui@gmail.com>
2018-03-27 00:44:55 -0300

    Add theme-color from localstorage

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -1,14 +1,14 @@
 ;(function (window, document, undefined) {
 	var nightModeStorage = localStorage.getItem('gmtNightMode');
 	var nightMode = document.querySelector('#night-mode');
+	var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
 	if (nightModeStorage) {
 		document.documentElement.classList.add('night-mode');
+		metaThemeColor.setAttribute("content", '#2b2b2b');
         nightMode.checked = true;
 	}
 
-	var nightMode = document.querySelector('#night-mode');
-	var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
 	// When clicked, toggle night mode on or off
 	nightMode.addEventListener('click', function (event) {
