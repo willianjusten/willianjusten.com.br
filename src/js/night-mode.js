1bc3232091e4b39dbc1fce4145cdfe897cb70abc
Willian Justen <willianjustenqui@gmail.com>
2018-03-27 00:19:42 -0300

    Polish night mode script

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -1,25 +1,17 @@
 ;(function (window, document, undefined) {
-	'use strict';
-	if (!('localStorage' in window)) return;
-	var nightMode = localStorage.getItem('gmtNightMode');
-    var nightMode = document.querySelector('#night-mode');
-	if (nightMode) {
-		document.documentElement.className += ' night-mode';
+	var nightModeStorage = localStorage.getItem('gmtNightMode');
+	var nightMode = document.querySelector('#night-mode');
+
+	if (nightModeStorage) {
+		document.documentElement.classList.add('night-mode');
         nightMode.checked = true;
 	}
-})(window, document);
-
-;(function (window, document, undefined) {
-
-	// Feature test
-	if (!('localStorage' in window)) return;
 
 	var nightMode = document.querySelector('#night-mode');
-	if (!nightMode) return;
-
 	// When clicked, toggle night mode on or off
 	nightMode.addEventListener('click', function (event) {
 		document.documentElement.classList.toggle('night-mode');
+
 		if ( document.documentElement.classList.contains('night-mode') ) {
 			localStorage.setItem('gmtNightMode', true);
 			return;
