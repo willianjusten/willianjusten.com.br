4c18fdf2f1400a29e00229ad6050f01d8d2192bc
Willian Justen <willianjustenqui@gmail.com>
2018-03-27 00:39:46 -0300

    Change theme-color with night-mode

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -8,15 +8,19 @@
 	}
 
 	var nightMode = document.querySelector('#night-mode');
+	var metaThemeColor = document.querySelector("meta[name=theme-color]");
+
 	// When clicked, toggle night mode on or off
 	nightMode.addEventListener('click', function (event) {
 		document.documentElement.classList.toggle('night-mode');
 
 		if ( document.documentElement.classList.contains('night-mode') ) {
 			localStorage.setItem('gmtNightMode', true);
+			metaThemeColor.setAttribute("content", '#2b2b2b');
 			return;
 		}
 		localStorage.removeItem('gmtNightMode');
+		metaThemeColor.setAttribute("content", '#005f97');
 	}, false);
 
 })(window, document);
