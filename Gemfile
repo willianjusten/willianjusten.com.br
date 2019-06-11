41094a7ea504d59f584348e17df0bf19c7bb14d2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2019-06-11 20:13:57 -0300

    Delete everything! DESTROYYY MOTHERFUCKER!

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rake'
-gem 'rouge'
-gem 'jekyll'
-gem 'html-proofer'
-gem 'travis-lint'
-gem 'jekyll-paginate'
--- testlinks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-echo 'Testing travis...'
-bundle exec travis-lint
-bundle exec jekyll build
-bundle exec htmlproofer ./_site --allow-hash-href --http-status-ignore "0,403,999" --only-4xx --url-ignore "/linkedin.com/,/t.co/,/udemy.com/"
