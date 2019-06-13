e24fa983fed0c7f57465ccc8b537c1e3bf904a26
Willian Justen <willianjustenqui@gmail.com>
2019-06-13 17:04:10 -0300

    Add `styled-components`

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/willianjusten-icon.png`
       }
-    }
+    },
+    `gatsby-plugin-styled-components`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
