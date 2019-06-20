0ca3c923175da639fc2b10f868868d063b259749
Willian Justen <willianjustenqui@gmail.com>
2019-06-20 17:37:39 -0300

    Add slug to the data

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,49 +4,42 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = (props) => {
-  const postList = props.data.allMarkdownRemark;
+const IndexPage = props => {
+  const postList = props.data.allMarkdownRemark
   return (
     <Layout>
       <SEO title="Home" />
-      {postList.edges.map(({ node }, i) => (
-        <div className="post-list">
-          <h1>{node.frontmatter.title}</h1>
-          <span>{node.frontmatter.date}</span>
-          <p>{node.frontmatter.description}</p>
-          <div>
-            <div dangerouslySetInnerHTML={{ __html: node.html }} />
+      {postList.edges.map(({ node }) => (
+        <Link to={node.fields.slug}>
+          <div className="post-list">
+            <h1>{node.frontmatter.title}</h1>
+            <span>{node.frontmatter.date}</span>
+            <p>{node.frontmatter.description}</p>
           </div>
-        </div>
+        </Link>
       ))}
     </Layout>
   )
 }
 
-export default IndexPage;
+export default IndexPage
 
-export const listQuery = graphql`
-  query ListQuery {
-    allMarkdownRemark(sort: {fields: frontmatter___date}) {
-    edges {
-      node {
-        frontmatter {
-          title
-          date
-          tags
-          main_class
-          layout
-          introduction
-          description
-          twitter_text
-          image
-          color
-          categories
+export const PostListQuery = graphql`
+  query PostListQuery {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date
+            description
+            main_class
+            title
+          }
         }
-        timeToRead
-        html
       }
     }
-  }
   }
 `
