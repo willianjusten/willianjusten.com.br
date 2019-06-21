dffe5e8242a042be347e91eff7a5e98b11263001
Willian Justen <willianjustenqui@gmail.com>
2019-06-20 22:10:47 -0300

    Adjust to use `useStaticQuery`

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,14 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 
 import Profile from './Profile'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+const Layout = ({ children }) => {
+  const { site } = useStaticQuery(
+    graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -24,33 +24,29 @@ const Layout = ({ children }) => (
           }
         }
       }
-    `}
-    render={data => (
-      <>
+    `
+  )
+
+  return (
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0
+      }}
+    >
+      <Link to="/">
         <Profile
-          title={data.site.siteMetadata.title}
-          position={data.site.siteMetadata.position}
-          description={data.site.siteMetadata.description}
+          title={site.siteMetadata.title}
+          position={site.siteMetadata.position}
+          description={site.siteMetadata.description}
         />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+      </Link>
+      <main>{children}</main>
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
-    <div>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>{post.frontmatter.title}</h1>
+      <h2>{post.frontmatter.description}</h2>
+      <p>{post.frontmatter.date}</p>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+    </Layout>
   )
 }
 
 export const query = graphql`
-  query($slug: String!) {
+  query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        description
         title
       }
     }
