139f13c684cf372931f2deab8bc360aed1b2d1d5
Willian Justen <willianjustenqui@gmail.com>
2019-06-20 22:13:55 -0300

    Add AboutPage

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Sobre mim" />
+    <h1>Sobre mim</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ullam
+      incidunt voluptatem omnis praesentium distinctio veniam recusandae harum
+      aut, quisquam tempora temporibus porro reprehenderit dolore nulla ipsa
+      unde sunt dolores?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ullam
+      incidunt voluptatem omnis praesentium distinctio veniam recusandae harum
+      aut, quisquam tempora temporibus porro reprehenderit dolore nulla ipsa
+      unde sunt dolores?
+    </p>
+  </Layout>
+)
+
+export default AboutPage
