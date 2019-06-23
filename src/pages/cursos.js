c3c508e384aedafe17cc5885b83840c2c9882f99
Willian Justen <willianjustenqui@gmail.com>
2019-06-22 23:35:32 -0300

    Move `CursosImage` to the right folder

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import CursosImage from '../utils/CursosImage'
+import CursosImage from '../components/CursosImage'
 
 const cursosList = [
   {
