9bdc8f00d414f3b76cc4f84afad426e7b6226db6
Willian Justen <willianjustenqui@gmail.com>
2019-07-03 15:10:18 -0300

    Add styles to Pagination

--- src/components/Pagination/styled.js
+++ src/components/Pagination/styled.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components'
 
 export const PaginationWrapper = styled.section`
+  align-items: center;
+  color: ${props => props.theme.texts};
   display: flex;
+  padding: 1.5rem 3rem;
+  justify-content: space-between;
+
+  a {
+    color: ${props => props.theme.texts};
+    text-decoration: none;
+    transition: color 0.5s;
+
+    &:hover {
+      color: ${props => props.theme.highlight};
+    }
+  }
 `
