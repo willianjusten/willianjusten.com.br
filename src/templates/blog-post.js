1fabe9c58d8a45f615eebebe6669270fe6b37b01
Willian Justen <willianjustenqui@gmail.com>
2019-07-06 03:48:05 -0300

    Make disqus to laod only close to the end of the post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { graphql } from 'gatsby'
 
 import { Disqus } from 'gatsby-plugin-disqus'
@@ -23,6 +23,19 @@ export default ({ data }) => {
     title: post.frontmatter.title
   }
 
+  var load_disqus = false
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 500
+      ) {
+        load_disqus = true
+      }
+    })
+  })
+
   return (
     <Layout>
       <SEO
@@ -38,7 +51,7 @@ export default ({ data }) => {
       <MainContent>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
 
-        <Disqus config={disqusConfig} />
+        {load_disqus && <Disqus config={disqusConfig} />}
       </MainContent>
     </Layout>
   )
