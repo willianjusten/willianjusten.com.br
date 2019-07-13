ac091be5c77d027c44defbcc2fc37592b0a4dc84
Willian Justen <willianjustenqui@gmail.com>
2019-07-13 10:03:11 -0300

    Adjust scroll to have offset and faster speed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import 'lazysizes'
 require('prismjs/themes/prism-tomorrow.css')
 
 if (typeof window !== 'undefined') {
   // eslint-disable-next-line global-require
-  require('smooth-scroll')('a[href*="#"]')
+  var SmoothScroll = require('smooth-scroll')
+
+  new SmoothScroll('a[href*="#"]', {
+    speed: 200,
+    offset: 66 // size of the header (sidebar) when mobile
+  })
 }
 
 export const onServiceWorkerUpdateReady = () => {
