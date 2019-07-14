5d4bc326d39c65b363a87f1f6897107e85c8d731
Willian Justen <willianjustenqui@gmail.com>
2019-07-14 10:51:22 -0300

    Edit abcss

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -12,10 +12,10 @@ export const LayoutWrapper = styled.section`
 
 export const LayoutMain = styled.main`
   background: var(--background);
+  min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   transition: background 0.5s, color 0.5s;
   width: 100%;
-  min-height: 100vh;
 
   ${media.lessThan('large')`
     padding: 0 0 3rem 0;
