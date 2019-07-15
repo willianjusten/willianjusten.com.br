ff1902e267fdfab1dd749f2d6356ce5f2e746e18
Willian Justen <willianjustenqui@gmail.com>
2019-07-15 11:36:12 -0300

    Fix link 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import GlobalStyles from '../styles/global'
 
 const Container = styled.section`
   align-items: center;
-  background-image: url('https://willianjusten.com/assets/img/john-404.gif');
+  background-image: url('https://willianjusten.com.br/assets/img/john-404.gif');
   background-position: bottom left;
   background-repeat: no-repeat;
   background-size: 800px;
