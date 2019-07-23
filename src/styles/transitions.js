bfdaa09d749145e3722b2ecc03ce5caab57f3077
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-07-23 14:02:14 -0300

    style(format): format file and add .editorconfig

--- src/styles/transitions.js
+++ src/styles/transitions.js
@@ -1,10 +1,10 @@
-const defaultTiming = '0.5s';
-const bgTransition = `background ${defaultTiming}`;
-const colorTransition = `color ${defaultTiming}`;
-const defaultTransition = `${bgTransition}, ${colorTransition}`;
+const defaultTiming = '0.5s'
+const bgTransition = `background ${defaultTiming}`
+const colorTransition = `color ${defaultTiming}`
+const defaultTransition = `${bgTransition}, ${colorTransition}`
 
 export default {
-    DEFAULT: defaultTransition,
-    COLOR: colorTransition,
-    BACKGROUND: bgTransition
-};
+  DEFAULT: defaultTransition,
+  COLOR: colorTransition,
+  BACKGROUND: bgTransition
+}
