cb6cad500ec480ba3f2f78f92382e7e6a2a4993e
Henrique <henriquepbdev@gmail.com>
2019-11-27 14:30:36 -0300

    Atualizando o texto do Stats (#138)
    * Atualizando o texto do Stats
    
    Alterando o texto exibido quando for de apenas 1 resultado a busca para o singular, antes quando achava apenas 1 post ele exibia no plural assim: "1 resultados encontrados em x ms" e agora exibe assim: "1 resultado encontrado em x ms".
    
    * Update index.js

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -32,7 +32,9 @@ const Search = props => {
             <Stats
               translations={{
                 stats(nbHits, timeSpentMS) {
-                  return `${nbHits} resultados encontrados em ${timeSpentMS}ms`
+                  return nbHits === 1
+              ? `${nbHits} resultado encontrado em ${timeSpentMS}ms`
+              : `${nbHits} resultados encontrados em ${timeSpentMS}ms`
                 }
               }}
             />
