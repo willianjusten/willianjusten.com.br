d53d00e687b987fd22b85d5facd19cdf77dfb9ff
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-26 19:39:13 -0300

    Fix coupon

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -8,7 +8,7 @@ const coursesList = [
   {
     title: 'React Avançado',
     image: 'react-avancado.png',
-    link: 'https://www.udemy.com/course/react-avancado/?couponCode=BLACKFRIDAY',
+    link: 'https://www.udemy.com/course/react-avancado/?couponCode=BLACKFRIDAYREACT',
     description: 'Crie aplicações reais com NextJS, Strapi, GraphQL e mais!'
   },
   {
