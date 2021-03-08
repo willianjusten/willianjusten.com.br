4925ce9168e3789f413771f4d72e05555c05f6e5
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-03-08 15:52:39 -0300

    Add new og:image

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -28,7 +28,7 @@ function SEO({ description, lang, meta, title, image }) {
   const metaDescription = description || site.siteMetadata.description
 
   const ogImage =
-    image || 'https://willianjusten.com.br/assets/img/blog-image.png'
+    image || 'https://willianjusten.com.br/assets/img/blog-image-new.png'
 
   return (
     <Helmet
