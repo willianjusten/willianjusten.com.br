8dc49cc7c9aaac2bf0f1607459af263707996056
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-07-04 15:06:28 -0300

    Add headings links remark

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -1,7 +1,14 @@
 import remark from 'remark'
 import html from 'remark-html'
+import headings from 'remark-autolink-headings'
+import slug from 'remark-slug'
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+    .use(html)
+    .use(slug)
+    .use(headings)
+    .process(markdown)
+
   return result.toString()
 }
