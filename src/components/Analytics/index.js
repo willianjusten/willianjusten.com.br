2a15794d080407ed7816993e18454666b015491d
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-07-04 00:23:50 -0300

    Change to use new G4A property

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -13,9 +13,8 @@ const Analytics = () => (
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+
+            gtag('config', '${GA_TRACKING_ID}');
           `
       }}
     />
