e48ea0dec48859f731a531980d8ba7e149fc949e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-11-01 13:22:23 -0300

    Add PROMONOV21 coupon react avançado

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -19,7 +19,7 @@ const coursesList = [
   {
     title: 'React Avançado',
     image: 'react-avancado.png',
-    link: 'https://www.udemy.com/course/react-avancado/?couponCode=PROMOINDEPENDENCIA',
+    link: 'https://www.udemy.com/course/react-avancado/?couponCode=PROMONOV21',
     description: 'Crie aplicações reais com NextJS, Strapi, GraphQL e mais!'
   },
   {
