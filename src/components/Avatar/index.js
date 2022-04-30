d229b964cbaa21b74319b2d74d68f87df53b5101
Willian Justen <willianjustenqui@gmail.com>
2022-04-30 11:11:40 -0300

    Update index.js

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -6,7 +6,7 @@ const Avatar = () => {
   return (
     <S.AvatarWrapper>
       <Image
-        src="https://pbs.twimg.com/profile_images/1465351725305327620/z44BYn1t_400x400.jpg"
+        src="https://github.com/willianjusten.png"
         alt="Uma foto minha vestido com o uniforme da Grifinória do Harry Potter"
         width={64}
         height={64}
