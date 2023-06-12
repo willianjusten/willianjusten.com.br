cd8b587bbb71b06258a9b540f9ff6a1d80149b01
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-12 11:26:15 -0300

    chore: Update React Avancado coupon

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -10,7 +10,7 @@ const coursesList = [
   },
   {
     title: 'React Avançado',
-    link: 'https://www.udemy.com/course/react-avancado/?couponCode=57A9D9C50F35F921463C',
+    link: 'https://www.udemy.com/course/react-avancado/?couponCode=9E454F41636A280AF968',
     description: 'Crie aplicações reais com NextJS, Strapi, GraphQL e mais!'
   },
   {
