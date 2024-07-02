bc3e23c0ac01e87a4144b13e365db2bc3215aa22
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-07-01 23:40:06 -0300

    chore: update markdown library

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,24 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { unified } from "unified";
+import parse from "remark-parse";
+import rehype from "remark-rehype";
+import headings from "rehype-autolink-headings";
+import slug from "rehype-slug";
+import stringify from "rehype-stringify";
+import rehypeShiki from "@shikijs/rehype";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(parse)
+    .use(rehype)
+    .use(rehypeShiki, {
+      theme: "one-dark-pro",
+    })
+    .use(slug)
+    .use(headings, {
+      behavior: "wrap",
+    })
+    .use(stringify)
+    .process(markdown);
+
   return result.toString();
 }
